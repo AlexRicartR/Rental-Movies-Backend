f3394e7d32740369e1b00b8c2f23603d7f461cc6
AlexRicartR <alex.ricart@outlook.com>
2022-11-06 21:29:55 +0100

    Update

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3006;
+const router = require('./router');
+const db = require('./db.js');
+const cors = require ('cors');
+
+// Middleware
+app.use(cors());
+app.use(express.json());  
+app.use(router);
+
+
+
+//Database connection
+db.then(()=>{
+    //Starting server
+        app.listen(port, ()=> console.log(`Node server running on http://localhost:${port}`));
+    })
+    .catch((err)=> console.log(err.message));
