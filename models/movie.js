53ee6fcbab6f6670a1b7c2acae05254868d5b132
AlexRicartR <alex.ricart@outlook.com>
2022-11-09 21:17:37 +0100

    Update movie.js

--- models/movie.js
+++ models/movie.js
@@ -1,22 +1,26 @@
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require ('../db/db');
-
-class Movies extends Model {
-
-
-
+'use strict';
+const {Model} = require('sequelize');
+// const category = require('./category');
+module.exports = (sequelize, DataTypes) => {
+  class movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      movie.belongsTo(models.category, {
+        foreignKey: 'categoryId'
+      });
+    }
+  };
+  movie.init({
+    title: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'movie',
+  });
+  return movie;
 };
-
-Movies.init({
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true,
-        validate: {
-            len: [5,50]
-        }
-    }, 
-
-
-
-});
