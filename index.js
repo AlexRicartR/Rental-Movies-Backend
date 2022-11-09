c2b0987306f63451bde3d85d35a1c1a2b7d0836a
AlexRicartR <alex.ricart@outlook.com>
2022-11-09 21:56:55 +0100

    Updating root files

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require ('express');
 const colors = require('colors');
 const morgan = require ('morgan');
-
+const logger = require('./config/winston')
+const db = require('./db/db')  //// Check if the extension does not affect project performance.
+const router = require('./router')
 
 
 const app = express();
 const router = require ('./router');
-const PORT = 3000;
+const PORT = 3000;  // Consider including process.env.PORT || for the interest of functionality
+
+app.use(morgan('combined', { stream: logger.stream }));
 app.use(express.json());
 
+app.get('/', (req, res) => {res.send('This is working')})
 app.use(router);
 
-app.listen(PORT, () => {
+db.then(() => {
+    app.listen(PORT, () => {
     console.log(`Server running through port ${PORT}`);
+})
 });
 
