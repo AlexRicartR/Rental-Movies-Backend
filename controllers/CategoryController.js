816626cebbfbed9fc0824fe5eb447b5ae01ad425
AlexRicartR <alex.ricart@outlook.com>
2022-11-12 15:23:25 +0100

    Update CategoryController.js

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -22,3 +22,25 @@ CategoryController.getAll = (req, res) => {
         });
       });
   };
+
+  //-------------------------------------------------------------------------------------
+//GET categories by Id from database
+CategoryController.getById = (req, res) => {
+    const id = req.params.id;
+  
+    category.findByPk(id)
+      .then(data => {
+        if (data) {
+          res.send(data);
+        } else {
+          res.status(404).send({
+            message: `Cannot find Category with id=${id}.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving categories with id=" + id
+        });
+      });
+  };
