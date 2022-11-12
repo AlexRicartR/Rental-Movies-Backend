92cd3624bbef2f95074b88750ed223868a31f793
AlexRicartR <alex.ricart@outlook.com>
2022-11-12 15:22:49 +0100

    Update CategoryController.js

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,5 +1,24 @@
 const db = require("../models");
 const category = db.category;
-const Op = db.Sequelize.Op; 
+const Op = db.Sequelize.Op; //Import all ORM sequelize functions 
 
-const CategoryController = {}; 
+const CategoryController = {}; //Create the object controller
+
+//CRUD end-points Functions
+//-------------------------------------------------------------------------------------
+//GET all categories from database
+CategoryController.getAll = (req, res) => {
+    const type = req.query.type;
+    var condition = type ? { type: { [Op.like]: `%${type}%` } } : null;
+  
+    category.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving categories."
+        });
+      });
+  };
