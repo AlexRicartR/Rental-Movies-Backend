c510208c643b60067402264f3904bc49762f7baf
AlexRicartR <alex.ricart@outlook.com>
2022-11-22 15:24:55 +0100

    Updating routes

--- router.js
+++ router.js
@@ -1,20 +1,16 @@
 
+const express = require('express');
+const router = express.Router();
 
+const UserRoutes = require('./views/usersRoutes');
+const filmRoutes = require('./views/filmRoutes');
+const seriesRoutes = require('./views/seriesRoutes');
+const loansRoutes = require('./views/loanRoutes');
 
-const router = require('express').Router();
+router.use('/users', UserRoutes);
+router.use('/films', filmRoutes);
+router.use('/series', seriesRoutes);
+router.use('/loans', loansRoutes )
 
-const auth = require('./middlewares/auth.middleware');
-
-//Importing routes as set forth in the views folder
-
-const MovieRouter = require('./views/MovieRouter');
-const CategoryRouter = require('./views/CategoryRouter');
-const UserRouter = require('./views/UserRouter');
-const SerieRouter = require('./views/SerieRouter');
-
-router.use('/api', UserRouter); 
-router.use('/movies', MovieRouter); 
-router.use('/categories', CategoryRouter);
-router.use('/series', SerieRouter);
 
 module.exports = router;
