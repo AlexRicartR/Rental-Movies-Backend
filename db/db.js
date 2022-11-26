fea112899515153238e8bf93056ba6a595ff1d04
AlexRicartR <alex.ricart@outlook.com>
2022-11-26 21:49:53 +0100

    Streamlining overall project structure

--- db/db.js
+++ db/db.js
@@ -1,16 +1,17 @@
 /// DB relocated from the root path
-
+const config = require('../config/config.json')
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
+    process.env.DB_DATABASE || config.development.database,
+    process.env.DB_USERNAME || config.development.username,
+    process.env.DB_PASSWORD || config.development.password,
+  
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT
+        host: process.env.DB_HOST || config.development.host,
+        port: process.env.DB_PORT || config.development.port,
+        dialect: process.env.DB_DIALECT || config.development.dialect
     }
 )
 
